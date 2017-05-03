416dbbaf00af7c10cde7802cbfaf7aa0a7d963d7
Rob Weber <rob.weber87@gmail.com>
2017-05-03 15:17:37 -0400

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description.to_json
+  end
+
   def edit
   end
 
